52fee461b906aab0fc372a4fd6b362ff3f147444
CarlosFariass <carlosfarias993@gmail.com>
2022-12-23 16:37:28 -0300

    solving the key error

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,21 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
       { type: 'link', content: 'jane.design/doctorcare'},
     ],
-    publishedAt: new Date('2022-12-22 16:36:00')
+    publishedAt: new Date('2022-12-22 16:36:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/CarlosFariass.png',
+      name: 'Carlos Farias',
+      role: 'Front-End Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-22 16:36:00'),
   },
 ]
 
@@ -33,18 +47,12 @@ export function App() {
         <main>
           {posts.map(post => {
             return (
-              <>
-                <Post 
+                <Post
+                  key={post.id} 
                   author={post.author}
                   content={post.content}
                   publishedAt={post.publishedAt}
                 />
-                <Post 
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-                />
-            </>
             )
           })}
         </main>
